1b31a85c62535935c8b74263a2f1f9a573fc70fe
Felipe Paulino <felipe.ribeiro@servexternos.gruposantander.com>
2023-03-17 17:59:54 -0300

    fix

---
+++ src/mock/userDetails.tsx
@@ -0,0 +1,242 @@
+
+"user": {
+    "username": "20000035344",
+    "email": "merchant.portal.ar@gmail.com",
+    "name": "Omar Enrique",
+    "displayName": "Omar Enrique Sivori",
+    "phone": "54 11 44519990",
+    "country": "AR",
+    "roles": [
+        "admin"
+    ]
+},
+"sellers": [
+    {
+        "seller_id": "4b679fe2-13bb-4356-9071-d8a39fe0d6fd",
+        "seller_code": "0000002636",
+        "trade_name": "Seller inscripto without fees20000035344",
+        "phone": {
+            "country_code": "54",
+            "area_code": "11",
+            "number": "44519990"
+        },
+        "contacts": [
+            {
+                "contact_id": "0e510d6d-c522-47fd-82ff-c74704364cc4",
+                "name": "Omar",
+                "surname": "Enrique Sivori",
+                "role": "contact",
+                "email": "merchant.portal.ar@gmail.com",
+                "phone": {
+                    "country_code": "54",
+                    "area_code": "11",
+                    "number": "44630007"
+                },
+                "cell_phone": {
+                    "country_code": "54",
+                    "area_code": "11",
+                    "number": "44519990"
+                },
+                "responsibilities": [
+                    "instalation",
+                    "chargebacks",
+                    "materials",
+                    "maintanence",
+                    "administrative",
+                    "financial"
+                ]
+            }
+        ],
+        "commercial_address": {
+            "street": "9 E 63 Y 64",
+            "number": "1536",
+            "suite": " - ",
+            "city": "La Plata",
+            "state": "AR-B",
+            "country": "AR",
+            "postal_code": "B1900ZZZ"
+        },
+        "situation": "approved",
+        "affiliations": [
+            "0000002636"
+        ],
+        "with_paybylink": false,
+        "commercial_information": {
+            "currency": "ARS",
+            "gross_monthly_income": 250000,
+            "gross_monthly_digital_payments": 250000,
+            "average_ticket": 250000
+        }
+    },
+    {
+        "seller_id": "34e7a234-475f-4041-b467-8a0d6a90e1cf",
+        "seller_code": "0000002700",
+        "trade_name": "seller 2 pruebas transferencia333",
+        "phone": {
+            "country_code": "54",
+            "area_code": "11",
+            "number": "44519990"
+        },
+        "contacts": [
+            {
+                "contact_id": "383ce9a6-ab13-45db-91c9-ae6caf536669",
+                "name": "Omar",
+                "surname": "Enrique Sivori",
+                "role": "contact",
+                "email": "merchant.portal.ar@gmail.com",
+                "phone": {
+                    "country_code": "54",
+                    "area_code": "11",
+                    "number": "44630007"
+                },
+                "cell_phone": {
+                    "country_code": "54",
+                    "area_code": "11",
+                    "number": "44519990"
+                },
+                "responsibilities": [
+                    "instalation",
+                    "chargebacks",
+                    "materials",
+                    "maintanence",
+                    "administrative",
+                    "financial"
+                ]
+            }
+        ],
+        "commercial_address": {
+            "street": "9 E 63 Y 64",
+            "number": "1536",
+            "suite": " - ",
+            "city": "La Plata",
+            "state": "AR-B",
+            "country": "AR",
+            "postal_code": "B1900ZZZ"
+        },
+        "situation": "approved",
+        "affiliations": [
+            "0000002700"
+        ],
+        "with_paybylink": false,
+        "commercial_information": {
+            "currency": "ARS",
+            "gross_monthly_income": 100000,
+            "gross_monthly_digital_payments": 100000,
+            "average_ticket": 100000
+        }
+    },
+    {
+        "seller_id": "1128422a-4d9f-4c6a-908d-ca9db3ed033a",
+        "seller_code": "0000002921",
+        "trade_name": "seller N 02",
+        "phone": {
+            "country_code": "54",
+            "area_code": "11",
+            "number": "44519990"
+        },
+        "contacts": [
+            {
+                "contact_id": "be7f6fcf-24b3-4eb9-a293-88bc95d0866a",
+                "name": "Omar",
+                "surname": "Enrique Sivori",
+                "role": "contact",
+                "email": "merchant.portal.ar@gmail.com",
+                "phone": {
+                    "country_code": "54",
+                    "area_code": "11",
+                    "number": "44630007"
+                },
+                "cell_phone": {
+                    "country_code": "54",
+                    "area_code": "11",
+                    "number": "44519990"
+                },
+                "responsibilities": [
+                    "instalation",
+                    "chargebacks",
+                    "materials",
+                    "maintanence",
+                    "administrative",
+                    "financial"
+                ]
+            }
+        ],
+        "commercial_address": {
+            "street": "9 E 63 Y 64",
+            "number": "1536",
+            "suite": " - ",
+            "city": "La Plata",
+            "state": "AR-B",
+            "country": "AR",
+            "postal_code": "B1900ZZZ"
+        },
+        "situation": "approved",
+        "affiliations": [
+            "0000002921"
+        ],
+        "with_paybylink": false,
+        "commercial_information": {
+            "currency": "ARS",
+            "gross_monthly_income": 250000,
+            "gross_monthly_digital_payments": 250000,
+            "average_ticket": 250000
+        }
+    },
+    {
+        "seller_id": "71e3e4b3-83d9-418d-a994-18848187cb19",
+        "seller_code": "0000003197",
+        "trade_name": "seller nuevo",
+        "phone": {
+            "country_code": "54",
+            "area_code": "11",
+            "number": "44519990"
+        },
+        "contacts": [
+            {
+                "contact_id": "00574e2b-2187-4eda-ac1a-190aa4a27bfd",
+                "name": "Omar",
+                "surname": "Enrique Sivori",
+                "role": "contact",
+                "email": "merchant.portal.ar@gmail.com",
+                "phone": {
+                    "country_code": "54",
+                    "area_code": "11",
+                    "number": "44630007"
+                },
+                "cell_phone": {
+                    "country_code": "54",
+                    "area_code": "11",
+                    "number": "44519990"
+                },
+                "responsibilities": [
+                    "instalation",
+                    "chargebacks",
+                    "materials",
+                    "maintanence",
+                    "administrative",
+                    "financial"
+                ]
+            }
+        ],
+        "commercial_address": {
+            "street": "9 E 63 Y 64",
+            "number": "1536",
+            "suite": " - ",
+            "city": "La Plata",
+            "state": "AR-B",
+            "country": "AR",
+            "postal_code": "B1900ZZZ"
+        },
+        "situation": "approved",
+        "affiliations": [
+            "0000003197"
+        ],
+        "with_paybylink": false,
+        "commercial_information": {
+            "currency": "ARS",
+            "gross_monthly_income": 250000,
+            "gross_monthly_digital_payments": 250000,
+            "average_ticket": 250000
+        }
+    }
+]
